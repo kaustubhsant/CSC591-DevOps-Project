d70b089ca08998b6b4b96b997c944b255fe1039d
aditya <abambre@ncsu.edu>
2015-10-01 18:23:11 -0400

    adding complete setup file with all issues fixed

--- scripts/jenkins-build.sh
+++ scripts/jenkins-build.sh
@@ -1,9 +1,35 @@
-!# /bin/sh
-sudo apt-get update
-sudo apt-get install git 
+#!/bin/bash
+
+
+# Install Jenkins dependency and start Jenkins
 sudo wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt-get update
-sudo apt-get install jenkins
+sudo apt-get update -y 
+sudo apt-get install jenkins git -y
+git config --global http.sslverify false 
 sudo service jenkins start
-chkconfig jenkins on
+
+sudo sh -c "echo \"jenkins ALL=(ALL) NOPASSWD:ALL\" >> /etc/sudoers"
+
+#Install python and pip package manager
+sudo apt-get install python -y
+sudo apt-get install python-pip -y
+
+#Install proxy which can redirect calls to Jenkin Service End points
+sudo apt-get install apache2 -y 
+sudo a2enmod proxy
+sudo a2enmod proxy_http
+
+sudo sh -c "echo \"<VirtualHost *:80>
+    ServerName ec2-52-89-232-35.us-west-2.compute.amazonaws.com
+    ProxyRequests Off
+    <Proxy *>
+        Order deny,allow
+        Allow from all
+    </Proxy>
+    ProxyPreserveHost on
+    ProxyPass / http://localhost:8080/
+</VirtualHost>\" > /etc/apache2/sites-available/jenkins.conf"
+
+sudo a2ensite jenkins
+sudo service apache2 reload
