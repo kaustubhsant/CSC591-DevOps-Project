38db9409502243d4c69cdd72fbcbc3a473f0bd13
Kaustubh Sant <kaustubhsant10@gmail.com>
2015-10-01 16:45:51 -0400

    Adding jenskins setup script

---
+++ scripts/jenkins-build.sh
@@ -0,0 +1,9 @@
+!# /bin/sh
+sudo apt-get update
+sudo apt-get install git 
+sudo wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install jenkins
+sudo service jenkins start
+chkconfig jenkins on
