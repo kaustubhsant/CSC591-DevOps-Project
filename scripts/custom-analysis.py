5bace8d490fd73c94cfe09e6933d4d1964dfebc7
Kaustubh Sant <kaustubhsant10@gmail.com>
2015-10-23 19:24:47 -0400

    Updating custom-analysis.py

--- scripts/custom-analysis.py
+++ scripts/custom-analysis.py
@@ -21,15 +21,17 @@ class ConstructorAnalyser:
 			return None
 
 	def check_for_constructor(self,infile):
-		self.parse(infile)
-		self.class_definitions = [node for node in self.module.body if isinstance(node, ast.ClassDef)]
-		if not self.class_definitions:
-			return
-		for c in self.class_definitions:
-			self.function_definitions = [node.name for node in c.body if isinstance(node, ast.FunctionDef)]			
-			if "__init__" not in self.function_definitions:
-				print("No constructor defined for class {0} in {1}".format(c.name,infile))
-
+		try:
+			self.parse(infile)
+			self.class_definitions = [node for node in self.module.body if isinstance(node, ast.ClassDef)]
+			if not self.class_definitions:
+				return
+			for c in self.class_definitions:
+				self.function_definitions = [node.name for node in c.body if isinstance(node, ast.FunctionDef)]			
+				if "__init__" not in self.function_definitions:
+					print("No constructor defined for class {0} in {1}".format(c.name,infile))
+		except:
+			pass
 		
 if __name__ == "__main__":
 	analyser = ConstructorAnalyser()
