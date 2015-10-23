863b7aabbb1a2acfd024eb6007a9d1c1164e290d
Kaustubh Sant <kaustubhsant10@gmail.com>
2015-10-22 21:50:36 -0400

    Updating postbuild script

--- scripts/post-build.sh
+++ scripts/post-build.sh
@@ -1,6 +1,8 @@
-python tests/run_tests.py
+mkdir htmlcov
+sudo python tests/run_tests.py
 coverage erase
-coverage run tests/run_tests.py
+sudo coverage run --branch tests/run_tests.py
 coverage report --include=*saws/*
+coverage html -d htmlcov
 python -m coverage xml --include=*saws/*
-pylint -f parseable -d I0011,R0801 saws | tee pylint.out
+pylint -f parseable -d I0011,R0801 saws | tee pylint.out 
